d72943ef3b629e115bd516800664b74defc09b96
gabrielaspooky <gabrielanava01@gmail.com>
2024-06-28 21:31:46 +0200

    added new variables

---
+++ main.js
@@ -0,0 +1,10 @@
+import { coffeeData } from "./data.js";
+
+
+const selectedProducts = [];
+
+document.getElementsByClassName("addButton").addEventListener("cli")
+
+addButton.addEventListener("click", () => {
+    divConteinArraySelectedProducts.innerHTML = "";
+  });
