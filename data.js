353fc45fb422397f09ce9e439a1add543da8fe5f
gabrielaspooky <gabrielanava01@gmail.com>
2024-06-28 19:47:13 +0200

    added new names to files and created data

---
+++ data.js
@@ -0,0 +1,84 @@
+
+import Colombia from "./assets/Coffee_bag_Colombia.png";
+import CostaRica from "./assets/Coffee_bag_Costa_Rica.png";
+import Laos from "./assets/Coffee_bag_Laos.png";
+import Etiopia from "./assets/Coffee_bag_Etiopia.png";
+import Kenia from "./assets/Coffee_bag_Kenia.png";
+import EtiopiaSidamo from "./assets/Coffee_bag_Etiopia_Sidamo.png";
+import CostaRicaMonteBello from "./assets/Coffee_bag_Costa_Rica_Monte_Bello.png";
+import ColombiaLaCasita from "./assets/Coffee_bag_Colombia_LaCasita.png";
+
+export const coffeeData = [
+    {
+    "id": "08a2b01071f7418b84ce1dcac4933a21",
+    "productName": "Costa Rica Tarrazú",
+    "isAvailable": true,
+    "coffeePrice": 9,
+    "img": CostaRica
+    },
+
+
+    {
+    "id": "08a2b01071f7418b84ce1dcac4933a22",
+    "productName": "Colombia Los Naranjos",
+    "isAvailable": true,
+    "coffeePrice": 9,
+    "img": Colombia
+    },
+
+    {
+        "id": "08a2b01071f7418b84ce1dcac4933a23",
+        "productName": "Laos Amanecer",
+        "isAvailable": true,
+        "coffeePrice": 9,
+        "img": Laos
+    
+    },
+
+    {
+        "id": "08a2b01071f7418b84ce1dcac4933a24",
+        "productName": "Etiopía Yrgacheff",
+        "isAvailable": true,
+        "coffeePrice": 9,
+        "img": Etiopia
+    
+    },
+
+    {
+        "id": "08a2b01071f7418b84ce1dcac4933a25",
+        "productName": "Kenia Ndunduri",
+        "isAvailable": true,
+        "coffeePrice": 15,
+        "img": Kenia
+    
+    },
+
+    {
+        "id": "08a2b01071f7418b84ce1dcac4933a26",
+        "productName": "Etiopía Sidamo",
+        "isAvailable": true,
+        "coffeePrice": 17,
+        "img": EtiopiaSidamo
+    
+    },
+
+    {
+        "id": "08a2b01071f7418b84ce1dcac4933a27",
+        "productName": "Costa Rica Monte Bello",
+        "isAvailable": true,
+        "coffeePrice": 12,
+        "img": CostaRicaMonteBello
+    
+    },
+
+    {
+        "id": "08a2b01071f7418b84ce1dcac4933a28",
+        "productName": "Colombia La Casita",
+        "isAvailable": false,
+        "coffeePrice": 9,
+        "img": ColombiaLaCasita
+    
+    },
+]
+    console.log(coffeeData);
+                        
